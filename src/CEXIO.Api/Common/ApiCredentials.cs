d66328dca6296f84c7a390aa228d5702437494b7
SERKAN BIRCAN <serkan@siniradam.net>
2017-06-16 22:09:40 +0300

    added ApiCredentials

---
+++ src/CEXIO.Api/Common/ApiCredentials.cs
@@ -0,0 +1,43 @@
+﻿using CEXIO.Api.Helpers;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace CEXIO.Api.Common
+{
+    public sealed class ApiCredentials
+    {
+        private readonly HMAC _hmac;
+
+        public string UserId { get; private set; }
+        public string ApiKey { get; private set; }
+
+        /// <summary>
+        /// You need credentials to access private API 's.
+        /// To see where can you find API informations
+        /// <seealso cref="https://cex.io/trade/profile#/api"/>
+        /// </summary>
+        /// <param name="userId">CEX.IO user id</param>
+        /// <param name="apiKey">Api key</param>
+        /// <param name="apiSecret">Api secret</param>
+        public ApiCredentials(string userId, string apiKey, string apiSecret)
+        {
+            UserId = userId;
+            ApiKey = apiKey;
+
+            _hmac = new HMACSHA256(Encoding.UTF8.GetBytes(apiSecret));
+            
+        }
+
+        public string GenerateSignature(out long nonce)
+        {
+            nonce = Nonce.Next();
+
+            var bytes = Encoding.UTF8.GetBytes($"{nonce}{UserId}{ApiKey}");
+            var hash = _hmac.ComputeHash(bytes);
+
+            return string.Concat(hash.Select(b => b.ToString("X2")));
+        }
+
+    }
+}
