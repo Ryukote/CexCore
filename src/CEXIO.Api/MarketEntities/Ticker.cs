4e6e13636a5fa7e20e8c692873a31275b3018ee6
SERKAN BIRCAN <serkan@siniradam.net>
2017-06-16 23:02:58 +0300

    property access modifiers changed to internal.

--- src/CEXIO.Api/MarketEntities/Ticker.cs
+++ src/CEXIO.Api/MarketEntities/Ticker.cs
@@ -9,26 +9,26 @@ namespace CEXIO.Api.MarketEntities
         public Timestamp Timestamp { get; internal set; }
 
         [JsonProperty("ask")]
-        public decimal Ask { get; set; }
+        public decimal Ask { get; internal set; }
 
         [JsonProperty("bid")]
-        public decimal Bid { get; set; }
+        public decimal Bid { get; internal set; }
 
         [JsonProperty("high")]
-        public decimal High { get; set; }
+        public decimal High { get; internal set; }
 
         [JsonProperty("low")]
-        public decimal Low { get; set; }
+        public decimal Low { get; internal set; }
 
         [JsonProperty("last")]
-        public decimal Last { get; set; }
+        public decimal Last { get; internal set; }
 
 
         [JsonProperty("volume")]
-        public decimal Volume24H { get; set; }
+        public decimal Volume24H { get; internal set; }
 
         [JsonProperty("volume30d")]
-        public decimal Volume30d { get; set; }
+        public decimal Volume30d { get; internal set; }
 
         // TODO: Symbol Pair must be add.
 
