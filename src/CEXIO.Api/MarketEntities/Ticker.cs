8d35381c408486d4ef8759bdbdf63333bef276a8
SERKAN BIRCAN <serkan@siniradam.net>
2017-06-16 22:10:50 +0300

    added  Ticker

---
+++ src/CEXIO.Api/MarketEntities/Ticker.cs
@@ -0,0 +1,36 @@
+﻿using CEXIO.Api.Helpers;
+using Newtonsoft.Json;
+
+namespace CEXIO.Api.MarketEntities
+{
+    public class Ticker
+    {
+        [JsonProperty("timestamp")]
+        public Timestamp Timestamp { get; internal set; }
+
+        [JsonProperty("ask")]
+        public decimal Ask { get; set; }
+
+        [JsonProperty("bid")]
+        public decimal Bid { get; set; }
+
+        [JsonProperty("high")]
+        public decimal High { get; set; }
+
+        [JsonProperty("low")]
+        public decimal Low { get; set; }
+
+        [JsonProperty("last")]
+        public decimal Last { get; set; }
+
+
+        [JsonProperty("volume")]
+        public decimal Volume24H { get; set; }
+
+        [JsonProperty("volume30d")]
+        public decimal Volume30d { get; set; }
+
+        // TODO: Symbol Pair must be add.
+
+    }
+}
