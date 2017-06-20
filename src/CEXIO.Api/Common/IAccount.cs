5cd993d8b09d1bec1a7aef721807af98fb74f299
SERKAN BIRCAN <serkan@siniradam.net>
2017-06-20 15:18:06 +0300

    added account & client interfaces.

---
+++ src/CEXIO.Api/Common/IAccount.cs
@@ -0,0 +1,21 @@
+﻿using CEXIO.Api.MarketEntities;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CEXIO.Api.Common
+{
+    public interface IAccount
+    {
+        Task<Balance> GetBalanceAsync();
+        Task<IEnumerable<Order>> GetOpenOrdersAsync(SymbolPairs pair, CancellationToken? cancellationToken = null);
+        Task<bool> CancelOrder(ulong orderId, CancellationToken? cancellationToken = null);
+        Task<bool> CancelOrder(SymbolPairs pair, CancellationToken? cancellationToken = null);
+        Task<Order> PlaceLimitOrder(Order order, CancellationToken? cancellationToken = null);
+        Task<Order> PlaceMarketOrder(Order order, CancellationToken? cancellationToken = null);
+        Task<long> OpenPosition(Position position, CancellationToken? cancellationToken = null);
+        Task<string> GetAddressAsync(Symbols symbol, CancellationToken? cancellationToken = null);
+        Task<IEnumerable<Position>> GetOpenPositonsAsync(SymbolPairs pair, CancellationToken? cancellationToken = null);
+        Task<bool> ClosePosition(SymbolPairs pair, ulong positionId, CancellationToken? cancellationToken = null);
+    }
+}
