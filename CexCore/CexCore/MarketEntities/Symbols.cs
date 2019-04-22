c0b5db31cfb1b0f6af9cf104264f8230461ab67f
Ryukote <ryukote@outlook.com>
2019-04-22 19:52:27 +0200

    Added support for OMG currency.

--- CexCore/CexCore/MarketEntities/Symbols.cs
+++ CexCore/CexCore/MarketEntities/Symbols.cs
@@ -14,7 +14,10 @@
         XRP,
         ZEC,
         BTG,
-        XLM
+        XLM,
+        QASH,
+        GUSD,
+        OMG
     }
 
     public enum SymbolPairs
@@ -47,6 +50,13 @@
         ZEC_BTC,
         BTG_USD,
         BTG_EUR,
-        BTG_BTC
+        BTG_BTC,
+        QASH_BTC,
+        QASH_ETH,
+        GUSD_USD,
+        GUSD_EUR,
+        OMG_USD,
+        OMG_EUR,
+        OMG_BTC
     }
 }
