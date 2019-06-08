7d278b78cbac580132074d5683c919d504885110
Ryukote <ryukote@outlook.com>
2019-06-09 01:12:16 +0200

    HttpClient for CEX.IO endpoints
    Added interface and implementation for HttpClient that handles CEX.IO endpoints.

---
+++ CexCore/Contracts/ICexHttpClient.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace CexCore.Contracts
+{
+    /// <summary>
+    /// Interface for CexHttpClient
+    /// </summary>
+    public interface ICexHttpClient
+    {
+        Task<Tuple<HttpStatusCode, string>> CexGetAsync(string url);
+        Task<Tuple<HttpStatusCode, string>> CexPostAsync(string url, HttpContent httpContent);
+    }
+}
