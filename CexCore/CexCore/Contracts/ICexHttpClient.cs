264eece387123f184b25a421655364a61a6bb362
Ryukote <ryukote@outlook.com>
2019-06-11 07:38:02 +0200

    Custom http client for CEX.IO
    Added interface for http client, constants that represents exception messages and implementation of http client interface.

---
+++ CexCore/CexCore/Contracts/ICexHttpClient.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace CexCore.Contracts
+{
+    /// <summary>
+    /// Interface for CexHttpClient
+    /// </summary>
+    public interface ICexHttpClient
+    {
+        Task<Tuple<HttpStatusCode, string>> GetAsync(string url);
+        Task<Tuple<HttpStatusCode, string>> PostAsync(string url, HttpContent httpContent);
+    }
+}
